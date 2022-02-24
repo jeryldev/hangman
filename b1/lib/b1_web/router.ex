14c89aedb67ac82d28582bf45a34f12d40d52db0
jeryldev <jeryldev@gmail.com>
2022-02-24 10:31:07 +0800

    add hangman router with new function

--- b1/lib/b1_web/router.ex
+++ b1/lib/b1_web/router.ex
@@ -18,6 +18,7 @@ defmodule B1Web.Router do
     pipe_through :browser
 
     get "/", HangmanController, :index
+    post "/", HangmanController, :new
   end
 
   # Other scopes may use custom stacks.
