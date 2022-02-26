de9a71a920d2f30b4025879a54da532a1cdfde6d
jeryldev <jeryldev@gmail.com>
2022-02-26 13:38:02 +0800

    add the hangman dependency and remove unused files

--- b2/lib/b2_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule B2Web.PageController do
-  use B2Web, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- b2/mix.exs
+++ b2/mix.exs
@@ -43,7 +43,8 @@ defmodule B2.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:hangman, path: "../hangman"}
     ]
   end
 
